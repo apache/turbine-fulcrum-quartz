70024691c84d00ee1db48f3f7be9bb13849faa30
Jeffery Painter <painter@apache.org>
2018-12-11 17:06:14 +0000

    Figured out testing error was due to missing quartz.xml - adding to test source and errors went away
    git-svn-id: https://svn.apache.org/repos/asf/turbine/fulcrum/trunk/quartz@1848699 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/fulcrum/quartz/DefaultQuartzSchedulerImplTest.java
+++ src/test/org/apache/fulcrum/quartz/DefaultQuartzSchedulerImplTest.java
@@ -19,6 +19,9 @@
 
 package org.apache.fulcrum.quartz;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
 import java.util.Date;
 import java.util.List;
 import java.util.Set;
@@ -28,7 +31,6 @@ import org.apache.fulcrum.quartz.test.SimpleJob;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.quartz.DateBuilder;
-import org.quartz.JobBuilder;
 import org.quartz.JobDetail;
 import org.quartz.JobKey;
 import org.quartz.Scheduler;
@@ -39,125 +41,111 @@ import org.quartz.TriggerKey;
 import org.quartz.impl.matchers.GroupMatcher;
 
 /**
- * @author <a href="mailto:leandro@ibnetwork.com.br">Leandro Rodrigo Saad Cruz</a>
+ * @author <a href="mailto:leandro@ibnetwork.com.br">Leandro Rodrigo Saad
+ *         Cruz</a>
  * @author <a href="mailto:epughNOSPAM@opensourceconnections.com">Eric Pugh </a>
  */
-public class DefaultQuartzSchedulerImplTest extends BaseQuartzTestCase
-{
+public class DefaultQuartzSchedulerImplTest extends BaseQuartzTestCase {
 
-    /**
-     * Make sure that the Quartz scheduler is up and running
-     * 
-     * @throws Exception generic exception
-     */
+	/**
+	 * Make sure that the Quartz scheduler is up and running
+	 * 
+	 * @throws Exception generic exception
+	 */
 	@Test
-    public void testService() throws Exception
-    {
-        Scheduler scheduler = quartz.getScheduler();
-        Assert.assertNotNull(scheduler);
-        Assert.assertNotNull(scheduler.getContext());
-    }
-	
+	public void testService() throws Exception {
+		Scheduler scheduler = quartz.getScheduler();
+		Assert.assertNotNull(scheduler);
+		Assert.assertNotNull(scheduler.getContext());
+	}
 
-    /**
-     * The following test has been updated for Quartz 2.3.0
-     * Adding/removing a scheduled job
-     * @throws Exception generic exception
-     */
-    @Test
-    public void testAddRemoveTrigger() throws Exception
-    {
-        Scheduler scheduler = quartz.getScheduler();
-        Assert.assertNotNull(scheduler);
-        Assert.assertNotNull(scheduler.getContext());
-        
-    	// Define job instance
-    	JobDetail job1 = JobBuilder.newJob(SimpleJob.class)
-    	    .withIdentity("simpleJob", "TURBINE")
-    	    .build();
+	/**
+	 * The following test has been updated for Quartz 2.3.0 Adding/removing a
+	 * scheduled job
+	 * 
+	 * @throws Exception generic exception
+	 */
+	@Test
+	public void testAddRemoveTrigger() throws Exception {
 
-    	// Define a Trigger that will fire "now", and not repeat
-    	Trigger trigger = TriggerBuilder.newTrigger()
-    	    .withIdentity("someTrigger", "TURBINE")
-    	    .startNow()
-    	    .build();
+		Scheduler scheduler = quartz.getScheduler();
+		TriggerKey triggerKey = TriggerKey.triggerKey("someTrigger", "TURBINE");
 
-    	// Schedule the job with the trigger
-    	scheduler.scheduleJob(job1, trigger);
+		Date date = DateBuilder.dateOf(0, 0, 0, 1, 1, 2099);
 
-    	TriggerKey triggerKey = new TriggerKey("someTrigger", "TURBINE");
-        Trigger t1 = scheduler.getTrigger(triggerKey);
-        
-        // System.out.println(" >> KEY: " + t1.getJobKey().toString());
-        
-        Assert.assertNotNull(t1);
-        scheduler.unscheduleJob(triggerKey);
-        trigger = scheduler.getTrigger(triggerKey);
-        Assert.assertNull(trigger);
-    }
-	
-	
-//
-//    /**
-//     * Get all scheduled jobs for "TURBINE" to make sure that
-//     * the registration worked.
-//     * @throws Exception generic exception 
-//     */
-//    @Test
-//    public void testGetJobs() throws Exception
-//    {
-//        Scheduler scheduler = quartz.getScheduler();
-//        Assert.assertNotNull(scheduler);
-//        Set<JobKey> jobNames = scheduler.getJobKeys(GroupMatcher.jobGroupEquals("TURBINE"));
-//        Assert.assertEquals("Expected two registered jobs", 2, jobNames.size());
-//    }
-//    
-//
-//    /**
-//     * Get the job details and job data map of an existing job to
-//     * make sure that the XStream configuration works.
-//     * @throws Exception generic exception
-//     */
-//    @Test
-//    public void testJobDetailMap() throws Exception
-//    {
-//    	
-//        JobDetail jobDetail = quartz.getScheduler().getJobDetail(JobKey.jobKey("simpleJob", "TURBINE"));
-//        Assert.assertNotNull(jobDetail);
-//        Assert.assertEquals("simpleJob", jobDetail.getKey().getName());
-//        Assert.assertNotNull(jobDetail.getJobDataMap());
-//        Assert.assertEquals(2, jobDetail.getJobDataMap().size());
-//    }
-//
-//    /**
-//     * Make sure the "notSoSimpleJob" is triggered by the CronTrigger.
-//     * @throws Exception generic exception
-//     */
-//    @Test
-//    public void testGetTriggersOfJob() throws Exception
-//    {
-//        List<? extends Trigger> triggers = quartz.getScheduler().getTriggersOfJob(JobKey.jobKey("notSoSimpleJob", "TURBINE"));
-//        Assert.assertEquals(1, triggers.size());
-//        Assert.assertEquals("cronTrigger", ((Trigger)triggers.get(0)).getKey().getName());
-//    }
-//
-//    /**
-//     * Make sure that our two registered jobs are executed after
-//     * one second.
-//     * @throws Exception generic exception
-//     */
-//    @Test
-//    public void testJobExecution() throws Exception
-//    {
-//        Thread.sleep(2000);
-//        Assert.assertTrue("SimpleJob was not executed", SimpleJob.wasExecuted);
-//        Assert.assertTrue("NotSoSimpleJob was not executed", NotSoSimpleJob.wasExecuted);
-//        Assert.assertTrue("NotSoSimpleJob was not serviced", NotSoSimpleJob.wasServiced);
-//        SimpleJob.reset();
-//        NotSoSimpleJob.reset();
-//        Thread.sleep(2000);
-//        Assert.assertTrue("SimpleJob was not executed", SimpleJob.wasExecuted);
-//        Assert.assertTrue("NotSoSimpleJob was not executed", NotSoSimpleJob.wasExecuted);
-//        Assert.assertTrue("NotSoSimpleJob was not serviced", NotSoSimpleJob.wasServiced);
-//    }
+		Trigger someDay = TriggerBuilder.newTrigger().withIdentity(triggerKey).forJob("simpleJob", "TURBINE")
+				.withSchedule(SimpleScheduleBuilder.simpleSchedule().withIntervalInHours(1).repeatForever())
+				.startAt(date).build();
+
+		scheduler.scheduleJob(someDay);
+
+		Trigger trigger = scheduler.getTrigger(triggerKey);
+		assertNotNull(trigger);
+		scheduler.unscheduleJob(triggerKey);
+		trigger = scheduler.getTrigger(triggerKey);
+		assertNull(trigger);
+
+	}
+
+	/**
+	 * Get all scheduled jobs for "TURBINE" to make sure that the registration
+	 * worked.
+	 * 
+	 * @throws Exception generic exception
+	 */
+	@Test
+	public void testGetJobs() throws Exception {
+		Scheduler scheduler = quartz.getScheduler();
+		Assert.assertNotNull(scheduler);
+		Set<JobKey> jobNames = scheduler.getJobKeys(GroupMatcher.jobGroupEquals("TURBINE"));
+		Assert.assertEquals("Expected two registered jobs", 2, jobNames.size());
+	}
+
+	/**
+	 * Get the job details and job data map of an existing job to make sure that the
+	 * XStream configuration works.
+	 * 
+	 * @throws Exception generic exception
+	 */
+	@Test
+	public void testJobDetailMap() throws Exception {
+
+		JobDetail jobDetail = quartz.getScheduler().getJobDetail(JobKey.jobKey("simpleJob", "TURBINE"));
+		Assert.assertNotNull(jobDetail);
+		Assert.assertEquals("simpleJob", jobDetail.getKey().getName());
+		Assert.assertNotNull(jobDetail.getJobDataMap());
+		Assert.assertEquals(2, jobDetail.getJobDataMap().size());
+	}
+
+	/**
+	 * Make sure the "notSoSimpleJob" is triggered by the CronTrigger.
+	 * 
+	 * @throws Exception generic exception
+	 */
+	@Test
+	public void testGetTriggersOfJob() throws Exception {
+		List<? extends Trigger> triggers = quartz.getScheduler()
+				.getTriggersOfJob(JobKey.jobKey("notSoSimpleJob", "TURBINE"));
+		Assert.assertEquals(1, triggers.size());
+		Assert.assertEquals("cronTrigger", ((Trigger) triggers.get(0)).getKey().getName());
+	}
+
+	/**
+	 * Make sure that our two registered jobs are executed after one second.
+	 * 
+	 * @throws Exception generic exception
+	 */
+	@Test
+	public void testJobExecution() throws Exception {
+		Thread.sleep(2000);
+		Assert.assertTrue("SimpleJob was not executed", SimpleJob.wasExecuted);
+		Assert.assertTrue("NotSoSimpleJob was not executed", NotSoSimpleJob.wasExecuted);
+		Assert.assertTrue("NotSoSimpleJob was not serviced", NotSoSimpleJob.wasServiced);
+		SimpleJob.reset();
+		NotSoSimpleJob.reset();
+		Thread.sleep(2000);
+		Assert.assertTrue("SimpleJob was not executed", SimpleJob.wasExecuted);
+		Assert.assertTrue("NotSoSimpleJob was not executed", NotSoSimpleJob.wasExecuted);
+		Assert.assertTrue("NotSoSimpleJob was not serviced", NotSoSimpleJob.wasServiced);
+	}
 }
